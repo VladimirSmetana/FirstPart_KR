35c087f43d9a2b45510e0db74319fa7f7a396a02
Владимир Сметана <SmetanaVladimir7@gmail.com>
2022-10-03 00:36:38 +0400

    Again

--- main.py
+++ main.py
@@ -2,16 +2,15 @@ from PyQt5 import QtWidgets
 from form import Ui_MainWindow
 import sys
 
-
-
-
 class mywindow(QtWidgets.QMainWindow):
     def __init__(self):
         super(mywindow, self).__init__()
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
+        self.ui.pushButton_2.clicked.connect(self.progexit)
 
-
+    def progexit(self):
+        exit()
 app = QtWidgets.QApplication([])
 application = mywindow()
 application.show()
